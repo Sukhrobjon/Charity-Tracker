703865851f056ef11c61ba8adc57ab22441c4db8
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-10-15 13:35:41 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+var exphbs = require('express-handlebars');
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req, res) => {
+    res.render('home', { msg: 'Handlebars are Cool!' });
+    console.log("Handlebars are")
+})
+
+
+// port
+app.listen(3000, () => {
+    console.log('App listening on port 3000!')
+})
