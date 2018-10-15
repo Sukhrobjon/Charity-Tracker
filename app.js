167bcf3f5ff06d9faff9eeaa99aae026e240a4c1
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-10-15 16:05:35 -0700

    Added MongoDB and Charity model

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/charityContracter')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -13,16 +14,30 @@ app.set('view engine', 'handlebars');
 // })
 
 // OUR MOCK ARRAY OF PROJECTS
-let charities = [
-    { title: "Great Review", charityTitle: "Batman II" },
-    { title: "Awesome Movie", charityTitle: "Titanic" }
-]
+const Charity = mongoose.model('Charity', {
+    title: String,
+    charityTitle: String
+});
+// let charities = [
+//     { title: "Great Review", charityTitle: "Batman II" },
+//     { title: "Awesome Movie", charityTitle: "Titanic" }
+// ]
 
 // INDEX
+// app.get('/', (req, res) => {
+//     res.render('charities-index', { charities: charities });
+// });
 app.get('/', (req, res) => {
-    res.render('charities-index', { charities: charities });
-});
-
+    Charity.find()
+        .then(charities => {
+            res.render('charities-index', {
+                charities: charities
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
 
 // port
 app.listen(3000, () => {
